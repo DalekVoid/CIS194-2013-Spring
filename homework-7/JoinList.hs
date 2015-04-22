60ccba14f7c9f0f1bf8f42fa28f9309383c26a0f
Dalek Void <dalek@voidamplifier.com>
2015-04-22 20:51:50 +0800

    ex 4 of hw 7 on the road

--- homework-7/JoinList.hs
+++ homework-7/JoinList.hs
@@ -1,8 +1,14 @@
 module JoinList where
 
+import Scrabble
+
+import Sized
+import Data.Monoid
+
 data JoinList m a = Empty
                   | Single m a
-                  | Append m (JoinListBasic a) (JoinListBasic a)
+                  | Append m (JoinList m a) (JoinList m a)
+  deriving (Show, Eq)
 
 -- Exercise 1
 tag :: Monoid m => JoinList m a -> m
@@ -33,27 +39,33 @@ indexJ idx (Single siz a)
   | otherwise  = Nothing
   -}
 indexJ idx (Append siz jl1 jl2)
-  | idx >= siz  = Nothing
+  | idx >= (getSize . size) siz = Nothing
   | idx >= size1 = indexJ (idx - size1) jl2
-  | otherwise   = indexing idx jl1
-    where size1 = getSize . tag jl1
+  | otherwise   = indexJ idx jl1
+    where size1 = getSize . size . tag $ jl1
+
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ 0 jl = jl
 dropJ _ Empty = Empty
 dropJ _ (Single _ _)   = Empty
 dropJ n (Append siz jl1 jl2)
-  | n >= siz = Nothing
-  | n >= size1 = dropJ (idx - size1) jl2
+  | n >= (getSize . size) siz = Empty
+  | n >= size1 = dropJ (n - size1) jl2
   | otherwise = dropJ n jl1 +++ jl2
-    where size1 = getSize . tag jl1
+    where size1 = getSize . size . tag $ jl1
 
 takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 takeJ 0 jl = Empty
-takeJ _ Empty = Empty 
+takeJ _ Empty = Empty
 takeJ _ sing@(Single _ _) = sing
 takeJ n jl@(Append siz jl1 jl2)
-  | n >= siz = jl
-  | n >= size1 = jl1 +++ takeJ (idx - size1) jl2
+  | n >= (getSize . size) siz = jl
+  | n >= size1 = jl1 +++ takeJ (n - size1) jl2
   | otherwise = takeJ n jl1
-    where size1 = getSize . tag jl1
+    where size1 = getSize . size . tag $ jl1
+
+-- Exercise 3
+scoreLine :: String -> JoinList Score String
+scoreLine str = Single (scoreString str) str
+
---
+++ homework-7/Scrabble.hs
@@ -0,0 +1,35 @@
+module Scrabble where
+
+import Data.List (foldl1')
+import Data.Maybe (fromMaybe)
+import Data.Char (toLower)
+import Control.Arrow (first)
+import qualified Data.Map as M
+
+letterScore :: M.Map Char Int
+letterScore = M.fromList (uppercaseScore ++ lowercaseScore)
+  where
+    uppercaseScore = concat [[(c, score) | c <- characters] | (characters, score) <- charactersScore]
+    lowercaseScore = map (first toLower) uppercaseScore
+    charactersScore = [ ("EAIONRTLSU", 1)
+                      , ("DG", 2)
+                      , ("BCMP", 3)
+                      , ("FHVWY", 4)
+                      , ("K", 5)
+                      , ("JX", 8)
+                      , ("QZ", 10)
+                      ]
+
+data Score = Score Int
+  deriving (Show, Eq)
+
+instance Monoid Score where
+  mempty = Score 0
+  (Score s1) `mappend` (Score s2) = Score (s1 + s2)
+
+
+score :: Char -> Score
+score char = Score . fromMaybe 0 $ M.lookup char letterScore
+
+scoreString :: String -> Score
+scoreString = mconcat . map score
