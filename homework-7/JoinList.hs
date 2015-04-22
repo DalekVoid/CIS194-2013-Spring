65215da29bc9f8b1190abce6ca53a92f83c31c5a
Dalek Void <dalek@voidamplifier.com>
2015-04-22 19:31:02 +0800

    HomeWork 7 redo on the way

---
+++ homework-7/JoinList.hs
@@ -0,0 +1,59 @@
+module JoinList where
+
+data JoinList m a = Empty
+                  | Single m a
+                  | Append m (JoinListBasic a) (JoinListBasic a)
+
+-- Exercise 1
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+jl1 +++ jl2 = Append (tag jl1 <> tag jl2) jl1 jl2
+
+-- Safe list indexing
+(!!?) :: [a] -> Int -> Maybe a
+[]     !!? _         = Nothing
+_      !!? i | i < 0 = Nothing
+(x:xs) !!? 0         = Just x
+(x:xs) !!? i         = xs !!? (i-1)
+
+-- Exercise 2
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty = Nothing
+-- if a is not a list, then the only possible value for it is 1
+indexJ idx (Single _ a)
+  | idx == 0  = Just a
+  | otherwise = Nothing
+{-
+indexJ idx (Single siz a)
+  | idx == siz = Just a
+  | otherwise  = Nothing
+  -}
+indexJ idx (Append siz jl1 jl2)
+  | idx >= siz  = Nothing
+  | idx >= size1 = indexJ (idx - size1) jl2
+  | otherwise   = indexing idx jl1
+    where size1 = getSize . tag jl1
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ 0 jl = jl
+dropJ _ Empty = Empty
+dropJ _ (Single _ _)   = Empty
+dropJ n (Append siz jl1 jl2)
+  | n >= siz = Nothing
+  | n >= size1 = dropJ (idx - size1) jl2
+  | otherwise = dropJ n jl1 +++ jl2
+    where size1 = getSize . tag jl1
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ 0 jl = Empty
+takeJ _ Empty = Empty 
+takeJ _ sing@(Single _ _) = sing
+takeJ n jl@(Append siz jl1 jl2)
+  | n >= siz = jl
+  | n >= size1 = jl1 +++ takeJ (idx - size1) jl2
+  | otherwise = takeJ n jl1
+    where size1 = getSize . tag jl1
